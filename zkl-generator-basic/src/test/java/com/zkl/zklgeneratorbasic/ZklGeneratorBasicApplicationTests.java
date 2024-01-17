ee4478d3ada1106f92b0f5d8d9e8c23a91c13d4e
zhangkunliang <1005789053@qq.com>
2024-01-17 17:02:34 +0800

    init

---
+++ zkl-generator-basic/src/test/java/com/zkl/zklgeneratorbasic/ZklGeneratorBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkl.zklgeneratorbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZklGeneratorBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
