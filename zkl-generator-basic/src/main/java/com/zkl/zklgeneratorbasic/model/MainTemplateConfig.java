d4078849e7b30fc43318ae2411e7bbc52a8e2c5d
zhangkunliang <1005789053@qq.com>
2024-01-24 15:51:02 +0800

    静态文件生成

---
+++ zkl-generator-basic/src/main/java/com/zkl/zklgeneratorbasic/model/MainTemplateConfig.java
@@ -0,0 +1,24 @@
+package com.zkl.zklgeneratorbasic.model;
+
+import lombok.Data;
+/**
+ * 动态模版配置
+ */
+@Data
+public class MainTemplateConfig {
+
+    /**
+     * 是否生成循环
+     */
+    private boolean loop;
+
+    /**
+     * 作者注释
+     */
+    private String author = "yupi";
+
+    /**
+     * 输出信息
+     */
+    private String outputText = "sum = ";
+}
